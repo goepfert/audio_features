59874d25b74aef84bfe6369a96406e44e029f856
Thomas Goepfert <thomas.goepfert@solderlab.de>
2020-05-04 20:44:45 +0200

    augmentation

--- imageAugmentation.js
+++ imageAugmentation.js
@@ -5,29 +5,74 @@
 
 'use strict';
 
-function createImage(buffer, fraction) {
-  const nRow = buffer.length;
-  let nCol = buffer[0].length;
-  let cutoff = nRow * fraction;
-  cutoff = Math.round(Math.random() * Math.round(cutoff));
+function createImageDataGenerator(opt) {
+  let options;
+  let def_options = {
+    fill_mode: 'nearest',
+  };
 
-  let newImage = [];
-
-  //left or right cutoff
-  const lr = Math.random();
-  if (lr < 0.5) {
-    newImage = buffer.slice(0, nRow - cutoff);
-    let zero = createZeroBuffer(cutoff, nCol);
-    newImage = zero.concat(newImage);
+  if (opt !== undefined) {
+    setOpt(opt);
   } else {
-    newImage = buffer.slice(cutoff, nRow);
-    let zero = createZeroBuffer(cutoff, nCol);
-    newImage = newImage.concat(zero);
+    // argument not passed or undefined
   }
 
-  return newImage;
-
-  function createZeroBuffer(nRows, nCols) {
-    return Array.from(Array(nRows), () => Array.from(Array(nCols), () => 0));
+  // https://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
+  function setOpt(opt) {
+    options = { ...def_options, ...opt };
   }
+
+  function getOpt() {
+    return options;
+  }
+
+  function horizontalShift(buffer, fraction) {
+    const nRow = buffer.length;
+    let nCol = buffer[0].length;
+    let cutoff = nRow * fraction;
+    cutoff = Math.round(Math.random() * Math.round(cutoff));
+
+    let newImage = [];
+    let fillBuffer = [];
+
+    //left or right cutoff
+    const lr = Math.random();
+
+    if (options.fill_mode == 'zero') {
+      fillBuffer = createZeroBuffer(cutoff, nCol);
+    } else if (options.fill_mode == 'nearest') {
+      if (lr < 0.5) {
+        fillBuffer = createNearestBuffer(cutoff, buffer[0]);
+      } else {
+        fillBuffer = createNearestBuffer(cutoff, buffer[nRow - 1]);
+      }
+    }
+
+    if (lr < 0.5) {
+      newImage = buffer.slice(0, nRow - cutoff);
+      newImage = fillBuffer.concat(newImage);
+    } else {
+      newImage = buffer.slice(cutoff, nRow);
+      newImage = newImage.concat(fillBuffer);
+    }
+
+    return newImage;
+
+    function createNearestBuffer(nRows, slice) {
+      let nearest = [];
+      for (let idx = 0; idx < nRows; idx++) {
+        nearest.push(Array.from(slice));
+      }
+      return nearest;
+    }
+
+    function createZeroBuffer(nRows, nCols) {
+      return Array.from(Array(nRows), () => Array.from(Array(nCols), () => 0));
+    }
+  }
+
+  return {
+    horizontalShift: horizontalShift,
+    getOpt: getOpt,
+  };
 }
